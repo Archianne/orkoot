5e68733ca3ca451ae3ef7082bd3dd951ec56244b
Archianne <helena19w@gmail.com>
2021-07-12 14:52:36 +0100

    .

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `;
+
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+`;
+
+export default function Home() {
+  return (
+    <main>
+      kkkkkk
+
+      Imagem
+      Bem vindo
+      Comunidades
+    </main>
+  );
+}
