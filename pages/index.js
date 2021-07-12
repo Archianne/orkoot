4f6a33aaee764c323f1608208f1aae016471e9ba
Archianne <helena19w@gmail.com>
2021-07-12 22:03:56 +0100

    first day

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import MainGrid from "../src/components/MainGrid";
 import Box from "../src/components/Box";
-import { AlurakutMenu } from "../src/lib/commons"
+import { SocialAreaBoxWrapper } from "../src/components/socialArea";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/commons";
 
 const ProfileSidebar = (prop) => {
   return (
     <Box>
       <img
-        src={`https://github.com/archianne.png`}
+        src={`https://github.com/${prop.githubUser}.png`}
         alt="Profile Picture"
         style={{ borderRadius: "8px" }}
       />
@@ -16,20 +17,42 @@ const ProfileSidebar = (prop) => {
 
 export default function Home() {
   const githubUser = "Archianne";
+  const gitFavourites = ["juunegreiros", "a", "b", "g", "d", "e"];
+
   return (
     <>
-    <AlurakutMenu />
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: "profileArea" }}></div>
-      <ProfileSidebar githubUser={githubUser} />
-      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
-        <Box>Bem vindo</Box>
-      </div>
-      <div className="socialArea" style={{ gridArea: "socialArea" }}>
-        <Box>Amigos</Box>
-        <Box>Comunidades</Box>
-      </div>
-    </MainGrid>
-  </>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}></div>
+        <ProfileSidebar githubUser={githubUser} />
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+          <h1 className="title">Bem Vindo</h1>
+          <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="socialArea" style={{ gridArea: "socialArea" }}>
+          <SocialAreaBoxWrapper>
+            <h2 className="smallTitle">Friends ({gitFavourites.length})</h2>
+            <ul>
+              {gitFavourites.map((friend) => {
+                return (
+                  <li>
+                    <a href={`/users/${friend}`} key={friend}>
+                      <img
+                        src={`https://github.com/${friend}.png`}
+                        alt="Friends Picture"
+                      />
+                      <span>{friend}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </SocialAreaBoxWrapper>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </>
   );
 }
--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -2,16 +2,23 @@ import styled from "styled-components";
 
 const MainGrid = styled.main`
   width: 100%;
-  display: grid;
   grid-gap: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
   padding: 16px;
-  grid-template-areas: "profileArea welcomeArea socialArea";
-  grid-template-columns: 160px 1fr 312px;
-
-  @media (max-width: 860px) {
-    display: flex;
-    flex-direction: column;
+  .profileArea {
+    display: none;
+    @media(min-width: 860px) {
+      display: block;
+    }
+  }
+  @media(min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: 
+      "profileArea welcomeArea socialArea";
+    grid-template-columns: 160px 1fr 312px;
   }
 `;
-
-export default MainGrid;
+export default MainGrid; 
